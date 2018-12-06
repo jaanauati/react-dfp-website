5d63caf0137fc38e3acf57432f05dd254d4239fd
Jonatan <barakawins@gmail.com>
2018-12-06 19:57:49 -0300

    more and more docs

--- components/ads/sizes/index.js
+++ components/ads/sizes/index.js
@@ -1,10 +1,12 @@
 import Example from './example';
 import code from '!raw-loader!./example'
+import Intro from './intro';
 import Hints from './hints';
 
 
 export {
     Example,
     Hints,
+    Intro,
     code,
 };
