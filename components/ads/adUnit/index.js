a19955b4654729af60f4c0a48e9381a7292c4f7e
Jonatan <barakawins@gmail.com>
2018-12-06 19:44:25 -0300

    add more docs

--- components/ads/adUnit/index.js
+++ components/ads/adUnit/index.js
@@ -1,10 +1,12 @@
 import Example from './example';
 import code from '!raw-loader!./example'
 import Hints from './hints';
+import Intro from './intro';
 
 
 export {
     Example,
     Hints,
+    Intro,
     code,
 };
---
+++ components/ads/dfpNetworkId/intro.js
@@ -0,0 +1,18 @@
+import { Fragment } from 'react';
+import Link from 'next/link';
+
+export default function(props) {
+    return (
+        <Fragment>
+            <p>
+              Use the property <strong>dfpNetworkId</strong> to configure the
+              dfp id you'd like to use in your app. There are possible ways in
+              which you can configure this prop:
+            </p>
+            <ol>
+                <li> via the DFPSlotsProvider component </li>
+                <li> or via AdSlot </li>
+            </ol>
+        </Fragment>
+    );
+}
