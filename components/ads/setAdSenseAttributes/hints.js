1510ddb488b6e7831eacbc017564decd4b6f05f2
Jonatan <barakawins@gmail.com>
2019-01-25 18:43:51 -0300

    setAdSenseAttributes docs

---
+++ components/ads/setAdSenseAttributes/hints.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default () => (
+  <ol>
+    <li>
+      Lines 15,18: we define two ad slots using the &lt;AdSlot&gt;
+      component.
+    </li>
+    <li>
+      Lines 7-8: we configure the AdSense attribute &apos;site_url&apos;
+      using the method
+      <br />
+      <strong>
+        {' DFPManager.configureSingleRequest() '}
+      </strong>
+      and load the ads in this page using
+      <br />
+      <strong>
+        <a href="/dfpmanager/load/">
+          {'DFPManager.load([id])'}
+        </a>
+      </strong>
+    </li>
+  </ol>
+);
