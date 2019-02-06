500111de5e206aa1f3a4e26a242cf2debdc8363f
Jonatan <barakawins@gmail.com>
2019-02-06 19:56:56 -0300

    updates docs, fix typos, update load(...)

--- components/ads/adSenseAttributes/hints.js
+++ components/ads/adSenseAttributes/hints.js
@@ -4,8 +4,9 @@ export default () => (
   <ol>
     <li> Lines 8: configure a global dfpNetworkId via &lt;DFPSlotProvider&gt;.</li>
     <li>
-      Line 10: we define an ad-slot using the &lt;AdSlot&gt; component.
-      We configure this slot to support the following sizes: 728x90 and 300x250.
+      Line 10: we define an ad-slot using the &lt;AdSlot&gt; component and
+      configure the AdSense attribute &apos;site_url&apos; using the property
+      <strong> adSenseAttributes </strong>.
     </li>
     <li>
       Line 13: we define another ad slot, in this case the slot supports the
--- components/ads/load/hints.js
+++ components/ads/load/hints.js
@@ -3,12 +3,20 @@ import React from 'react';
 export default () => (
   <ol>
     <li>
-      Lines 15,18: we define two ad slots using the &lt;AdSlot&gt;
+      Lines 19,22, 25: we define three ad slots using the &lt;AdSlot&gt;
       component.
     </li>
     <li>
-      Lines 7-9: manually load all the ad slots just 5 seconds after the
-      component is mounted.
+      Lines 7-9: manually load ad slots &apos;test1&apos; and
+      &apos;test2&apos; 5 seconds after the page loads. Notice that we
+      pass this ids when calling <strong> DFPManager.load(...args) </strong>.
+    </li>
+    <li>
+      Lines 10-12: Ten seconds after (that&apos;s fifteen after the page loads),
+      we initialize the rest of the available ads in the page,
+      in this case only &apos;test3&apos;. Notice that this is the default
+      behavior when not passing any arguments to&nbsp;
+      <strong> DFPManager.load(...args)</strong>.
     </li>
   </ol>
 );
