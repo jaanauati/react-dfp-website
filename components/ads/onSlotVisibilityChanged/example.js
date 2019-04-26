6ecf788d3335bd8a0b0d3a598d8ae873a10b26c7
Jonatan Alexis Anauati <barakawins@gmail.com>
2019-04-26 18:38:11 -0300

    fix configs (#23)

--- components/ads/onSlotVisibilityChanged/example.js
+++ components/ads/onSlotVisibilityChanged/example.js
@@ -5,18 +5,18 @@ import { DFPSlotsProvider, AdSlot } from 'react-dfp';
 export default class Example extends Component {
   render() {
     return (
-      <DFPSlotsProvider dfpNetworkId="4585">
+      <DFPSlotsProvider dfpNetworkId="9999">
         <div className="desktop-ads">
           <AdSlot
             sizes={[[728, 90], [300, 250]]}
-            adUnit="speedtest.net/stnext_leaderboard"
+            adUnit="homepage/1"
             onSlotVisibilityChanged={dfpEventData => console.log('Slot 1: visibility changed.', dfpEventData)}
           />
         </div>
         <div className="mobile-ads">
           <AdSlot
-            sizes={[[320, 50], [300, 50], [160, 600]]}
-            adUnit="speedtest.net/stnext_skyscraper"
+            sizes={[[320, 50], [300, 50]]}
+            adUnit="homepage/mobile"
             onSlotVisibilityChanged={dfpEventData => console.log('Slot 2: visibility changed.', dfpEventData)}
           />
         </div>
