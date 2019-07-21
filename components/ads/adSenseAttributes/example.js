93730dc725505b3e72792ae1736ce4579ff5a7df
Jonatan <barakawins@gmail.com>
2019-07-20 22:32:16 -0300

    Fixes for the adsense documentation.

--- components/ads/adSenseAttributes/example.js
+++ components/ads/adSenseAttributes/example.js
@@ -7,7 +7,7 @@ export default class Example extends Component {
     return (
       <DFPSlotsProvider dfpNetworkId="9999">
         <div className="desktop-ads">
-          <AdSlot adSenseAttributes={{'site_url': 'example.com'}} sizes={[[728, 90], [300, 250]]} adUnit="homepage/1" />
+          <AdSlot adSenseAttributes={{'page_url': 'example.com'}} sizes={[[728, 90], [300, 250]]} adUnit="homepage/1" />
         </div>
         <div className="mobile-ads">
           <AdSlot dfpNetworkId="1999" sizes={[[300, 250], [300, 600], 'fluid']} adUnit="homepage/mobile" />
--- components/ads/adSenseAttributes/hints.js
+++ components/ads/adSenseAttributes/hints.js
@@ -5,7 +5,7 @@ export default () => (
     <li> Lines 8: configure a global dfpNetworkId via <strong>&lt;DFPSlotProvider&gt;</strong>.</li>
     <li>
       Line 10: we define an ad-slot using the <strong>&lt;AdSlot&gt;</strong> component and
-      configure the AdSense attribute &apos;site_url&apos; using the property
+      configure the AdSense attribute &apos;page_url&apos; using the property
       <strong> adSenseAttributes </strong>.
     </li>
     <li>
