fc2bbd258bcde45fcf4278c55797f775d1806392
Jonatan <barakawins@gmail.com>
2020-02-28 19:27:47 -0300

    fix lazyload example (https://github.com/jaanauati/react-dfp/issues/182)

--- components/ads/lazyLoad/example.js
+++ components/ads/lazyLoad/example.js
@@ -9,14 +9,20 @@ export default class Example extends Component {
         singleRequest={false}
         dfpNetworkId="9999"
       >
-        <div className="desktop-ads">
-          <AdSlot sizes={[[728, 90]]} adUnit="sports" />
-        </div>
-        <div style={{ height: '300vh' }} />
-        <AdSlot sizes={['fluid']} adUnit="news" />
-        <div style={{ height: '900vh' }} />
-        <div className="mobile-ads">
-          <AdSlot sizes={[[160, 600]]} adUnit="weather" />
+        <div>
+          <div className="desktop-ads">
+            <AdSlot sizes={[[728, 90]]} adUnit="sports" />
+          </div>
+          <div style={{ height: '300vh' }} />
+          <AdSlot sizes={['fluid']} adUnit="news" />
+          <div style={{ height: '900vh' }} />
+          <div className="mobile-ads">
+            <AdSlot sizes={[[160, 600]]} adUnit="weather" />
+          </div>
+          <div style={{ height: '900vh' }} />
+          <div className="mobile-ads">
+            <AdSlot sizes={[[160, 600]]} adUnit="weather" />
+          </div>
         </div>
       </DFPSlotsProvider>
     );
