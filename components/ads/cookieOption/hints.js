1c243dc697280a13a3f90d5ec821a863e7d29837
Jonatan Alexis Anauati <barakawins@gmail.com>
2020-08-13 10:35:11 -0300

    Add cookieOption prop docs. (#52)

---
+++ components/ads/cookieOption/hints.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => (
+  <ol>
+    <li>
+      Line 8 configure default / global properties using the
+      <strong>&lt;DFPSlotProvider&gt;</strong>. We use the property&nbsp;
+      <strong>cookieOptions</strong>
+      &nbsp;to tell the library to ignore Google Ad Manager cookies.
+    </li>
+    <li> Line 10: define an ad-slot using the <strong>&lt;AdSlot&gt;</strong> component.</li>
+    <li> Line 13: define another ad-slot using the <strong>&lt;AdSlot&gt;</strong> component.</li>
+  </ol>
+);
